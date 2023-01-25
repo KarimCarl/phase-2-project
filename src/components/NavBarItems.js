f6ebd7d6543b46fc37dbf02c5b926b1da12ef5f8
KarimCarl <cndungu109@gmail.com>
2023-01-25 19:31:29 +0300

    Added menu items and logo

--- src/components/NavBarItems.js
+++ src/components/NavBarItems.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import {FaBars, FaTimes} from "react-icons/fa"
+import { useRef } from 'react';
+import "../App.css"
 
 function NavBarItems() {
+
+    const navRef = useRef();
+
+    const showNavBarItems = () => {
+        navRef.current.classList.toggle("responsive_nav")
+    }
+
   return (
-    <div>NavBarItems</div>
+    <header>
+        <h1>P E T F L I X</h1>
+        <nav>
+            {/* links to navigation bar */}
+            <a href="/#">HOME</a>
+            <a href="/#">PETS</a>
+            <a href="/#">ADOPT A PET</a>
+            <a href="/#">PAWS</a>
+
+            {/* closes nvigation bar in small screens*/}
+            <button className='nav-btn nav-close-btn' onClick={showNavBarItems}>
+                <FaTimes />
+            </button>
+        </nav>
+        
+        {/* button that opens navbar in small screens */}
+        <button className='nav-btn' onClick={showNavBarItems}> 
+            <FaBars />
+        </button>
+    </header>
   )
 }
 
-export default NavBarItems
+export default NavBarItems;
